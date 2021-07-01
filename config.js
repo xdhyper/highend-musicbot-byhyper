536155aaabcdebc290b88520e58b45331411884d
Hyper <80406549+xdhyper@users.noreply.github.com>
2021-07-01 15:13:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = {
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
   "24/7": true, //If you want the bot to be stay in the vc 24/7
-  CookieSecret: "Pikachu is cute", //A Secret like a password
+  CookieSecret: "Hyper Is Best", //A Secret like a password
   IconURL:
     "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
   Permissions: 2205280576, //Bot Inviting Permissions
